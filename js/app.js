fd59544da0228648b609da46eda295aceadbb938
Andrew556556 <144379643+Andrew556556@users.noreply.github.com>
2025-05-29 18:27:11 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ function fetchData() {
     if (isFetching) return; // Prevenir múltiples llamadas simultáneas
     isFetching = true;
 
-    fetch('http://3.239.217.148:5000/api/devices')
+    fetch('http://44.199.208.126:5000/api/devices')
         .then(response => {
             if (!response.ok) throw new Error('Error al obtener los datos');
             return response.json();
